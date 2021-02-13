ccba324089f6d7cf9fac416c25fffe6b1c859432
Elian Batista <elianbatista2000@gmail.com>
2021-02-13 12:18:18 -0300

    Configuração da Left Bar

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const Dashboard: React.FC = () => {
+  return (
+    <h1>
+      Dashboard
+    </h1>
+  )
+}
+
+export default Dashboard
---
+++ src/components/left-bar/LeftBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const LeftBar: React.FC = () => {
+  return (
+    <h1>
+      LeftBar
+    </h1>
+  )
+}
+
+export default LeftBar
