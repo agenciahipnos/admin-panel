cbc818db76b6519c5134d7704e4e90d8ed51c8df
Elian Batista <elianbatista2000@gmail.com>
2021-02-13 12:18:26 -0300

    Configuração da Topbar

---
+++ src/components/top-bar/TopBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './styles.scss'
+
+const TopBar: React.FC = () => {
+  return (
+    <header>
+      <div className="left-side">
+        <div className="logo">
+          <img src="/assets/img/logo/logo.png" alt="Logo Agência Hipnoss"/>
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="person">
+          
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default TopBar
