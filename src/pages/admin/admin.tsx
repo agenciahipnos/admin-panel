07ceced179fcf7412bbcd40d1545c832ae86ec3f
Elian Batista <elianbatista2000@gmail.com>
2021-02-17 21:00:43 -0300

    admin

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Dashboard from '../../components/dashboard/Dashboard'
 import LeftBar from '../../components/left-bar/LeftBar'
-import TopBar from '../../components/top-bar/TopBar'
 import './styles.scss'
 
 const Admin: React.FC = () => {
   return (
     <div className="wrapper">
-      <div className="topo">
-        <TopBar />
-      </div>
       <div className="content-body">
         <div className="left-side">
           <LeftBar />
